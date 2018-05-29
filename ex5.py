bca27b488fd09ba196c8893d390ae99677c511d3
Kelly Brower <kbrower77@gmail.com>
2018-05-28 23:37:39 -0700

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,5 +1,5 @@
 name = "Thelonius Monk"
-age = 74 #a bag of bullshit
+age = 74 #age
 height = (74 * 0.52) #bass frets in metric frets
 weight = (293 * 2.2) #pounds sterling
 eyes = 'Aeon'
@@ -7,11 +7,11 @@ teeth = 'Black'
 hair = 'Nightshade'
 
 print "Let's talk about %s." % name
-print "He's %d bass frets tall, not so much its centiclitoris's"  % height
-print "He's %d pounds no wait kigs babe heavy." % weight
-print "Actually that's now metric cigs babe."
+print "He's %d bass frets tall, not so much its centimeters"  % height
+print "He's %d pounds no wait kgs heavy." % weight
+print "Actually that's now metric tons bro."
 print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His teeth are usually %s depending on the coffee." % teeth
 
-# this line is a bag of dildos, so make sure to lube each one carefully
+# this line
 print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight)
