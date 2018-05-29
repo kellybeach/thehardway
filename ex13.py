47215b8603b131cd5117ecf8caca608d51ef4b19
Kelly Brower <kbrower77@gmail.com>
2018-05-28 21:11:10 -0700

    Add files via upload

---
+++ ex13.py
@@ -0,0 +1,14 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+#to run this script, you must write "python ex13.py" like normal,
+#followed by names for each variable, example; apple, orange, bananfluer
+
+age = raw_input("How old are you? ")
+print "So, you smoke %r banfluers a day?" % (age)
---
+++ ex5.py
@@ -0,0 +1,17 @@
+name = "Thelonius Monk"
+age = 74 #a bag of bullshit
+height = (74 * 0.52) #bass frets in metric frets
+weight = (293 * 2.2) #pounds sterling
+eyes = 'Aeon'
+teeth = 'Black'
+hair = 'Nightshade'
+
+print "Let's talk about %s." % name
+print "He's %d bass frets tall, not so much its centiclitoris's"  % height
+print "He's %d pounds no wait kigs babe heavy." % weight
+print "Actually that's now metric cigs babe."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+
+# this line is a bag of dildos, so make sure to lube each one carefully
+print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight)
